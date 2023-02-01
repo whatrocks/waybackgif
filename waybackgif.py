777cd23658feb040b3e830f942b801eeb04e1925
Charlie Harrington <charles.harrington@gmail.com>
2023-02-01 11:22:14 -0800

    lint

--- waybackgif.py
+++ waybackgif.py
@@ -1,4 +1,3 @@
-import requests
 from waybackpy import WaybackMachineCDXServerAPI
 from PIL import Image
 import imageio
@@ -8,14 +7,18 @@ from time import sleep
 def generate_wayback_gif(url):
     driver = webdriver.Chrome(executable_path="~/tmp/chromedriver")
 
-    api = WaybackMachineCDXServerAPI(url, "waybackgif", start_timestamp=2022, end_timestamp=2023)
+    api = WaybackMachineCDXServerAPI(
+        url,
+        "waybackgif",
+        start_timestamp=2022,
+        end_timestamp=2023
+    )
     snapshots = api.snapshots()
 
     screenshots = []
 
     count = 1
     for snapshot in snapshots:
-        print("snapshot: ", snapshot.archive_url)
         driver.get(snapshot.archive_url)
         sleep(1)
         filename = "snapshot" + str(count) + ".png"
@@ -25,8 +28,6 @@ def generate_wayback_gif(url):
 
     images = [Image.open(img) for img in screenshots]
     imageio.mimsave('wayback.gif', images, fps=2)
-
-    print('Gif saved as wayback.gif')
 
 print("start")
 generate_wayback_gif("https://www.charlieharrington.com")
