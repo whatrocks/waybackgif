f2b9c5dd649c5d3845b239d9255b14d0358bd199
Charlie Harrington <charles.harrington@gmail.com>
2023-02-01 16:27:14 -0800

    add examples

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # waybackgif
 create a gif of chronological screenshots from any website on Wayback Machine
+
+## Examples
+
+Annual snapshot of apple.com (1996 - 2023)
+[apple](apple.gif)
+
+Annual snapshot of stripe.com (2009 - 2023)
+[stripe](stripe.gif)
--- waybackgif.py
+++ waybackgif.py
@@ -5,12 +5,16 @@ from selenium import webdriver
 from time import sleep
 
 def generate_wayback_gif(url):
-    driver = webdriver.Chrome(executable_path="~/tmp/chromedriver")
+    options = webdriver.ChromeOptions()
+    options.page_load_strategy = 'normal'
+    driver = webdriver.Chrome(
+            executable_path="~/tmp/chromedriver",
+            options=options)
 
     api = WaybackMachineCDXServerAPI(
         url,
-        "waybackgif",
-        start_timestamp=2022,
+        "waybackgif agent",
+        start_timestamp=1995,
         end_timestamp=2023
     )
     snapshots = api.snapshots()
@@ -18,7 +22,14 @@ def generate_wayback_gif(url):
     screenshots = []
 
     count = 1
+    datemonth = {}
     for snapshot in snapshots:
+        if snapshot.statuscode != "200":
+            continue
+        date = str(snapshot.datetime_timestamp)[0:4]
+        if date in datemonth:
+            continue
+        datemonth[date] = True
         driver.get(snapshot.archive_url)
         sleep(1)
         filename = "snapshot" + str(count) + ".png"
@@ -27,8 +38,8 @@ def generate_wayback_gif(url):
         count += 1
 
     images = [Image.open(img) for img in screenshots]
-    imageio.mimsave('wayback.gif', images, fps=2)
+    imageio.mimsave('apple2.gif', images, fps=2)
 
 print("start")
-generate_wayback_gif("https://www.charlieharrington.com")
+generate_wayback_gif("www.apple.com")
 print("done")
