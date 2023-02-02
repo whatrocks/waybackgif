b37cc354a5f812b838b414752f48912d4a401e47
Charlie Harrington <charles.harrington@gmail.com>
2023-02-01 17:18:02 -0800

    add cli args for url start end

--- waybackgif.py
+++ waybackgif.py
@@ -3,8 +3,18 @@ from PIL import Image
 import imageio
 from selenium import webdriver
 from time import sleep
+import argparse
+import datetime
 
-def generate_wayback_gif(url):
+def generate_wayback_gif(
+    url,
+    start_year="2020", 
+    end_year=datetime.datetime.now().year
+):
+    print("Generating gif of " +
+    f"annual screenshots of {url} "+
+    f"from {start_year} - {end_year}")
+
     options = webdriver.ChromeOptions()
     options.page_load_strategy = 'normal'
     driver = webdriver.Chrome(
@@ -14,8 +24,8 @@ def generate_wayback_gif(url):
     api = WaybackMachineCDXServerAPI(
         url,
         "waybackgif agent",
-        start_timestamp=1995,
-        end_timestamp=2023
+        start_timestamp=f"{start_year}0101000000",
+        end_timestamp=f"{end_year}1231235959"
     )
     snapshots = api.snapshots()
 
@@ -38,8 +48,21 @@ def generate_wayback_gif(url):
         count += 1
 
     images = [Image.open(img) for img in screenshots]
-    imageio.mimsave('apple2.gif', images, fps=2)
+    imageio.mimsave("result.gif", images, fps=2)
 
-print("start")
-generate_wayback_gif("www.apple.com")
-print("done")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate a wayback gif')
+    parser.add_argument('url', type=str, help='A valid URL')
+    parser.add_argument(
+        '--start_year',
+        type=str,
+        help='start year (optional, default: 2020)',
+        default='2020')
+    parser.add_argument(
+        '--end_year', 
+        type=str, 
+        help='end year (optional, default: current year)', 
+        default=str(datetime.datetime.now().year))
+    args = parser.parse_args()
+
+    generate_wayback_gif(args.url, args.start_year, args.end_year)
